0e52beb3c99bf619dbbcedb24850dc41a2263663
KevMcEwan <kev_mcewan@hotmail.com>
2018-10-16 15:43:47 +0100

    divide function tested

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -11,5 +11,9 @@ def subtract(number1, number2)
 end
 
 def multiply(number1, number2)
-  return number1*number2
+  return number1 * number2
+end
+
+def divide(number1, number2)
+  return number1 / number2
 end
