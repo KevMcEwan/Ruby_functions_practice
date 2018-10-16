f75786b196ca10d43b1c3c19f2fb4fdc644b2457
KevMcEwan <kev_mcewan@hotmail.com>
2018-10-16 16:20:38 +0100

    test_number_to_full_name_month_1,3,9, functions tested

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -25,3 +25,37 @@ end
 def join_string( string_1, string_2)
   return string_1 + string_2
 end
+
+def add_string_as_number(number1, number2)
+  return number1.to_i + number2.to_i
+end
+
+def number_to_full_month_name(number1)
+if number1 == 1
+  return "January"
+elsif number1 == 2
+return "February"
+elsif number1 == 3
+return "March"
+elsif number1 == 4
+return "April"
+elsif number1 == 5
+return "May"
+elsif number1 == 6
+return "June"
+elsif number1 == 7
+return "July"
+elsif number1 == 8
+return "August"
+elsif number1 == 9
+return "September"
+elsif number1 == 10
+return "October"
+elsif number1 == 11
+return "November"
+elsif number1 == 12
+return "December"
+else
+  return "invalid"
+end
+end
